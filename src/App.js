4a97fc42284245a02c983a8d221519c4d1ae0961
Timothy Solomon <git@timosolo.me>
2018-01-17 14:22:08 +0200

    custom pics

--- src/App.js
+++ src/App.js
@@ -110,7 +110,7 @@ We’re really excited about what EngineOne can do for you!
             <Container>
             <Grid style={{height:'90vh'}} verticalAlign='middle' stackable>
             <Grid.Row>
-              <Grid.Column width={8}>
+              <Grid.Column width={6}>
 
               <Header
               as='h1'
@@ -126,10 +126,10 @@ We’re really excited about what EngineOne can do for you!
             </Header>
 
               </Grid.Column>
-              <Grid.Column width={8}>
+              <Grid.Column width={10}>
                 <Image
                   style={{width:'100%'}}
-                  src='team.png'
+                  src='team.svg'
                 />
               </Grid.Column>
             </Grid.Row> 
@@ -159,20 +159,24 @@ We’re really excited about what EngineOne can do for you!
         </Grid>
       </Segment>
 
-        <Segment style={{ padding: '8em 0em' }} vertical>
+        <Segment className="benifits" style={{ padding: '8em 0em' }} vertical>
           <Grid container stackable verticalAlign='middle'>
             <Grid.Row>
               <Grid.Column width={8}>
-                <Header as='h3'>Real-time Collaboration that lets you keep your favourite IDE</Header>
+                <img src="ide.svg" className="icon"/>
+                <Header as='h3'>
+                Real-time Collaboration that lets you keep your favourite IDE</Header>
                 <p>
                 Most attempts at real-time collaboration require you use <i>their</i> IDE some online code editor. 
                 EngineOne works with your existing IDE whether its TextPad or VS Code.
                 </p>
+                </Grid.Column>
+              <Grid.Column width={2}>
               </Grid.Column>
               <Grid.Column width={6}>
                 <Image
                   style={{width:'100%'}}
-                  src='https://www-assets0.herokucdn.com/assets/home/hero/focus_2x-315bdf6fd4e19d85ceaf800528afb680190b79056a23516428b4cb4ff61d37c2.png'
+                  src='ide-pic.svg'
                 />
               </Grid.Column>
             </Grid.Row>
@@ -181,10 +185,13 @@ We’re really excited about what EngineOne can do for you!
               <Grid.Column width={6}>
                 <Image
                   style={{width:'100%'}}
-                  src='https://www-assets3.herokucdn.com/assets/home/hero/apps_2x-1cbc7cbe49e12ad3cfb038f1b21942c2c4773ed159e422c1c10e5d2257fbdd80.png'
+                  src='safe-pic.svg'
                 />
+                </Grid.Column>
+              <Grid.Column width={2}>
               </Grid.Column>
               <Grid.Column width={8}>
+                <img src="safe.svg" className="icon"/>
                 <Header as='h3'>Your code is SAFE</Header>
                 <p>
                 As you work, EngineOne automatically syncs your changes securely to the cloud. 
@@ -196,28 +203,32 @@ We’re really excited about what EngineOne can do for you!
 
             <Grid.Row>
               <Grid.Column width={8}>
+                <img src="proactive.svg" className="icon"/>
                 <Header as='h3'>Advanced Warning System</Header>
                 <p>
                 Get notifications when colleagues are working on code that might break yours. 
                 Be proactive and avoid conflicts before they happen.
                 </p>
+                </Grid.Column>
+              <Grid.Column width={2}>
               </Grid.Column>
               <Grid.Column width={6}>
                 <Image
                   style={{width:'100%'}}
-                  src='https://www-assets2.herokucdn.com/assets/home/hero/ecosystem_2x-51bfd00540ebf0b1e52b4fd935222d5c46c7ad58fc05273ca96311788ac0777c.png'
+                  src='proactive-pic.svg'
                 />
               </Grid.Column>
             </Grid.Row>
 
             <Grid.Row>
-              <Grid.Column width={6}>
+              <Grid.Column width={8}>
                 <Image
                   style={{width:'100%'}}
-                  src='https://www-assets3.herokucdn.com/assets/home/hero/kafka_2x-996f59eddfe02e23b0bc4c7b5755f21e0a277366d05597c4268e8f8c662fd5ae.png'
+                  src='community-pic.svg'
                 />
-              </Grid.Column>
+                </Grid.Column>
               <Grid.Column width={8}>
+                <img src="community.svg" className="icon"/>
                 <Header as='h3'>Community Focussed</Header>
                 <p>
                 With EngineOne you are never left in the dark. We are community focussed, and always open to talk. 
@@ -230,7 +241,7 @@ We’re really excited about what EngineOne can do for you!
         </Segment>
 
 
-        <Segment  className="gradient" style={{ padding: '0em' }} vertical>
+        {/* <Segment  className="gradient" style={{ padding: '0em' }} vertical>
           <Grid celled='internally' columns='equal' stackable>
             <Grid.Row textAlign='center'>
               <Grid.Column >
@@ -238,13 +249,41 @@ We’re really excited about what EngineOne can do for you!
               </Grid.Column>
             </Grid.Row>
           </Grid>
-        </Segment>
+        </Segment> */}
 
         <Segment style={{ padding: '8em 0em' }} vertical>
+          <Grid container stackable verticalAlign='middle'>
+            <Grid.Row>
+              <Grid.Column width={8}>
+                <Image
+                  style={{width:'100%'}}
+                  src='screen.svg'
+                />
+                </Grid.Column>
+              <Grid.Column width={8}>
+                <Header as='h3'>How it Works</Header>
+                <p>
+                EngineOne is a code syncing service that helps Software Developers spend 
+                more time coding and less time managing code by automating their version 
+                control workflow
+                </p>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+
+        <Segment className="features" style={{ padding: '8em 0em' }} vertical>
+        
           <Grid columns='equal' container stackable verticalAlign='top'>
+          <Grid.Row>
+            <Grid.Column style={{textAlign:"center", paddingBottom:"2em"}} >
+              <Header as='h2'>Features</Header>
+            </Grid.Column>
+          </Grid.Row>
+
             <Grid.Row>
             <Grid.Column >
-              <Icon size="huge" name='signal' />
+              <img src="feature-offline.svg" />
               <Header as='h4'>Offline First</Header>
               <p>
               Your code change history is stored even if you are working offline. 
@@ -253,14 +292,14 @@ We’re really excited about what EngineOne can do for you!
               </p>
             </Grid.Column>
             <Grid.Column >
-              <Icon size="huge" name='warning sign' />
+            <img src="feature-detect.svg" />
               <Header as='h4'>Early Conflict Detection</Header>
               <p>
               Unlike other solutions, EngineOne alerts you of potential issues before they become big problems.
               </p>
             </Grid.Column>
               <Grid.Column >
-                <Icon size="huge" name='grid layout' />
+              <img src="feature-extend.svg" />
                 <Header as='h4'>Extendable</Header>
                 <p>
                 Plugins allow new features to be added to the EngineOne platform.
@@ -270,7 +309,7 @@ We’re really excited about what EngineOne can do for you!
 
             <Grid.Row>
             <Grid.Column >
-              <Icon size="huge" name='configure' />
+            <img src="feature-custom.svg" />
               <Header as='h4'>Customisable Workflow</Header>
               <p>
               EngineOne can be customised to assist with your version control workflow, whether you use git-flow or single-branch methodologies.
@@ -278,7 +317,7 @@ We’re really excited about what EngineOne can do for you!
               </p>
             </Grid.Column>
             <Grid.Column >
-              <Icon size="huge" name='compress' />
+            <img src="feature-integrate.svg" />
               <Header as='h4'>More Integrations</Header>
               <p>
               EngineOne works with your existing tools, rather than against them. It supports git, svn and Mercurial out the box. And with a powerful API, it can be integrated with task managers and CI tools and other 3rd Party tools.
@@ -293,7 +332,7 @@ We’re really excited about what EngineOne can do for you!
         </Segment>
 
 
-
+{/* 
         <Segment  className="gradient" style={{ padding: '0em' }} vertical>
           <Grid celled='internally' columns='equal' stackable>
             <Grid.Row textAlign='center'>
@@ -302,37 +341,22 @@ We’re really excited about what EngineOne can do for you!
               </Grid.Column>
             </Grid.Row>
           </Grid>
-        </Segment>
+        </Segment> */}
 
-        <Segment inverted vertical style={{ padding: '5em 0em' }}>
-          <Container>
-            <Grid divided inverted stackable>
-              <Grid.Row>
-                <Grid.Column width={3}>
-                  <Header inverted as='h4' content='More' />
-                  <List link inverted>
-                    <List.Item as='a'>Sitemap</List.Item>
-                    <List.Item as='a'>Contact Us</List.Item>
-                    <List.Item as='a'>Pricing</List.Item>
-                    <List.Item as='a'>Documentation</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column width={3}>
-                  <Header inverted as='h4' content='About' />
-                  <List link inverted>
-                    <List.Item as='a'>Meet the Team</List.Item>
-                    <List.Item as='a'>About Us</List.Item>
-                    <List.Item as='a'>Careers</List.Item>
-                    <List.Item as='a'>Favorite X-Men</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column width={7}>
-                  <Header as='h4' inverted>Footer Header</Header>
-                  <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Container>
+        <Segment className="footer" vertical style={{ padding: '0' }}>
+        <Container>
+        <Menu borderless secondary  size='small'>
+          <Menu.Item as='a'>Contact</Menu.Item>
+          <Menu.Item as='a'>Privacy</Menu.Item>
+          <Menu.Item as='a'>Terms of use</Menu.Item>
+          <Menu.Item as='a'>Accessability</Menu.Item>
+          <Menu.Item position='right'>
+            <Menu.Item as='a'><Icon name='twitter' /></Menu.Item>
+            <Menu.Item as='a'><Icon name='facebook' /></Menu.Item>
+            <Menu.Item as='a'><Icon name='linkedin' /></Menu.Item>
+          </Menu.Item>
+        </Menu>
+      </Container>
         </Segment>
       </div>
     )
